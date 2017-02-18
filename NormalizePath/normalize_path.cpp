b5efe2881ee47937ceac401fe6ee07b705959e64
Rostislav Ostapenko <rostislav.ostapenko@gmail.com>
2017-02-19 00:00:05 +0400

    Performance optimizations.

--- NormalizePath/normalize_path.cpp
+++ NormalizePath/normalize_path.cpp
@@ -1,4 +1,5 @@
 #include <cstdlib>
+#include <cstring>
 #include <ctime>
 #include <chrono>
 #include <iomanip>
@@ -30,10 +31,10 @@ std::string normalize(const std::string& path)
 
     struct folder
     {
-        folder(size_t b, size_t e): begin(b), end(e) {}
+        folder(size_t b, size_t s): begin(b), size(s) {}
 
         size_t begin;
-        size_t end;
+        size_t size;
     };
 
     // Possible optimization: assume that path length cannot be > PATH_MAX
@@ -45,60 +46,65 @@ std::string normalize(const std::string& path)
     // Number of subfolders is no more than path length / 2 + 1.
     folders.reserve(path.size() / 2 + 1);
 
-    size_t root_pos = 0;
-    size_t cur_pos = 0;
-    size_t prev_pos = 0;
+    size_t root_idx = 0;
+
+    const char* const cpath = path.c_str();
+    const size_t size = path.size();
+    const char* const cend = cpath + size;
+
+    const char* cur_pos = cpath;
+    const char* prev_pos = cpath;
 
     const auto add_folder =
-    [&path, &folders, &root_pos, &cur_pos, &prev_pos](size_t pos)
+    [cpath, cend, size, &folders, &root_idx, &cur_pos, &prev_pos](const char* pos)
     {
         const size_t len = pos - prev_pos;
+
         if (len == 0 ||
-            (len == 1 && (path[pos - 1] == rel_point)) ||
-            (len == 2 && path[pos - 1] == rel_point && path[pos - 2] == delimiter))
+            (len == 1 && *(pos - 1) == rel_point) ||
+            (len == 2 && *(pos - 1) == rel_point && *(pos - 2) == delimiter))
         {
             // Skip "", ".", "/." as empty subfolders
         }
-        else if ((len == 2 && path[pos - 1] == rel_point && path[pos - 2] == rel_point) ||
-            (len == 3 && path[pos - 1] == rel_point &&
-                path[pos - 2] == rel_point && path[pos - 3] == delimiter))
+        else if ((len == 2 && *(pos - 1) == rel_point && *(pos - 2) == rel_point) ||
+            (len == 3 && *(pos - 1) == rel_point && *(pos - 2) == rel_point &&
+                *(pos - 3) == delimiter))
         {
             // Remove previous subfolder for ".." and "/.." till reaching the root.
-            if (folders.size() > root_pos) folders.pop_back();
+            if (folders.size() > root_idx) folders.pop_back();
         }
         else
         {
             // Norm subfolder, add it.
-            folders.emplace_back(prev_pos, pos);
+            folders.emplace_back(prev_pos - cpath, pos - prev_pos);
 
             // Subfolder starts from string beginning but doesn't start with "/".
             // Hmmm, guess it's domain name and new root. Don't harass it by upcoming "..".
-            if (prev_pos == 0 && path[0] != delimiter) root_pos = 1;
+            if (prev_pos == cpath && *cpath != delimiter) root_idx = 1;
         }
 
         cur_pos = pos + 1;
 
         // Skip adjacent path delimiters ('////' -> '/')
-        while (cur_pos < path.size() && path[cur_pos] == delimiter) ++cur_pos;
+        while (cur_pos < cend && *cur_pos == delimiter) ++cur_pos;
 
         prev_pos = cur_pos - 1;
     };
 
-    size_t delim_pos = 0;
-    while ((delim_pos = path.find(delimiter, cur_pos)) != std::string::npos)
+    while (auto pos = std::strchr(cur_pos, delimiter))
     {
-        add_folder(delim_pos);
+        add_folder(pos);
     }
 
     // Process last subfolder separately.
-    add_folder(path.size());
+    add_folder(cend);
 
     std::string result;
-    result.reserve(path.size());
+    result.reserve(size);
 
     for (const auto& f : folders)
     {
-        result.append(path.begin() + f.begin, path.begin() + f.end);
+        result.append(cpath + f.begin, f.size);
     }
 
     return result;
@@ -185,7 +191,7 @@ void performance_test()
 
     std::cout << std::fixed << std::setprecision(2);
     std::cout << "Total duration " << total_time_us.count() << " us" << std::endl;
-    std::cout << "Average duration " << (total_time_us.count() / max_count) << " us" << std::endl;
+    std::cout << "Average single call duration " << (total_time_us.count() / max_count) << " us" << std::endl;
 
     const auto duration_sec = total_time_us.count() / 1E6;
     const auto total_size_mb = total_size / 1E6;
